3fff16302e6a858ccbe0f94976ee479465d64b07
Aman Sinha <113705491+EzioAman@users.noreply.github.com>
2026-02-22 07:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -352,7 +352,7 @@ if st.button("Update Stats"):
         # ✅ RATE LIMIT PROTECTION (ONLY AFTER CALLS)
         if processed % 5 == 0:
             with st.spinner("Cooling API requests..."):
-                time.sleep(45)
+                time.sleep(59)
 
 
     # ✅ ONE GOOGLE API UPDATE
@@ -502,5 +502,6 @@ for i,(p,s) in enumerate(rank.items(),1):
     """,unsafe_allow_html=True)
 
 st.markdown("</div>",unsafe_allow_html=True)
+
 
 
